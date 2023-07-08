76fa1620a79ee39107f76f5ebfb9ddf22a776f34
ranjal007 <ranjalomer12@gmail.com>
2023-07-08 17:08:51 +0530

    Files updarted

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import './CardWithHoverEffect.css'; 
 import {
   Card,
   CardContent,
@@ -9,6 +10,7 @@ import {
   CardMedia,
   withStyles,
 } from "@material-ui/core";
+import ServerDrivenUI from "./Card2";
 
 const styles = (theme) => ({
   card: {
@@ -65,7 +67,11 @@ const styles = (theme) => ({
 export default function CardComponent(props) {
   const { classes, id, user, title, category, notes } = props;
   const [contentJson, setContentJson] = useState({});
-
+  const cardStyles = {
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', // Adjust the values as per your preference
+    margin: '25px',
+    backgroundColor: '#f5f5f5'
+    };
   const handleClose = (notes) => {
     try {
       // const content_json =notes.replace("/\/g", "");
@@ -76,60 +82,73 @@ export default function CardComponent(props) {
       console.log(err);
     }
   };
+  const [isHovered, setIsHovered] = useState(false);
 
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   useEffect(() => {
     handleClose(notes);
   }, []);
 
   return (
-    <div style={{boxShadow: "0 2px 6px rgba(0,0,0,0.15)",padding:"16px",display:"flex",flexDirection:"column",width:'100%'}}>
-      {/* {console.log("Res",contentJson)} */}
-      <div  style={{display:"flex",flexDirection:"row",justifyContent:'space-between',backgroundColor:'#a7cad6',padding:"5px"}}>
-      <div style={{width:'20%'}}>{id}</div>
-      <div style={{width:'20%'}}>{user}</div>
-      <div style={{width:'20%'}}>{title}</div>
-      <div style={{width:'20%'}}>{category}</div>
-      </div>
-      
-      {contentJson.content ? (
-  <div>
-    {contentJson.content.map((blockgroup) => (
-      <div>
-        {/* <h1>{blockgroup.type}</h1> */}
-        {blockgroup.content ? (
-          <div>
-            {blockgroup.content.map((blockcontainer) => (
-              <div style={blockcontainer.attrs}>
-                {/* <h2>{blockcontainer.type}</h2> */}
-                {blockcontainer.content?<div>
-                    {blockcontainer.content.map((paragraph_heading)=>
+    <Card  style={{margin: '25px',cursor:'pointer'}}variant="outlined" elevation={3} className={`card-container ${isHovered ? 'lift-up' : ''}`}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}>
+      <CardContent>
+        <Typography variant="h5" component="h2" >
+          {title}
+        </Typography>
+        <Typography color="textSecondary">
+          Category: {category}
+        </Typography>
+      <ServerDrivenUI jsonData={contentJson}/>
+    
+      </CardContent>
+    </Card>
+    
+//   <div>
+//     {contentJson.content.map((blockgroup) => (
+//       <div>
+//         {/* <h1>{blockgroup.type}</h1> */}
+//         {blockgroup.content ? (
+//           <div>
+//             {blockgroup.content.map((blockcontainer) => (
+//               <div style={blockcontainer.attrs}>
+//                 {/* <h2>{blockcontainer.type}</h2> */}
+//                 {blockcontainer.content?<div>
+//                     {blockcontainer.content.map((paragraph_heading)=>
                     
-                    <div style={paragraph_heading.attrs}>
-                        {/* <h4>{paragraph_heading.type}</h4> */}
-                        {(paragraph_heading.type =='heading' && paragraph_heading.content) ?
+//                     <div style={paragraph_heading.attrs}>
+//                         {/* <h4>{paragraph_heading.type}</h4> */}
+//                         {(paragraph_heading.type =='heading' && paragraph_heading.content) ?
 
-                              <h6 style={paragraph_heading.attrs}>{paragraph_heading.content[0].text}</h6>:
+//                               <h6 style={paragraph_heading.attrs}>{paragraph_heading.content[0].text}</h6>:
 
-                              (paragraph_heading.type == 'paragraph' && paragraph_heading.content)?
+//                               (paragraph_heading.type == 'paragraph' && paragraph_heading.content)?
 
-                              <p style={paragraph_heading.attrs}>{paragraph_heading.content[0].text}</p> : null
+//                               <p style={paragraph_heading.attrs}>{paragraph_heading.content[0].text}</p> : null
 
 
-                        }
-                    </div>)}
-                </div>:null}
-              </div>
-            ))}
-          </div>
-        ) : null}
-      </div>
-    ))}
-  </div>
-) : (
-  null
-)}
+//                         }
+//                     </div>)}
+//                 </div>:null}
+//               </div>
+//             ))}
+//           </div>
+//         ) : null}
+//       </div>
+//     ))}
+//   </div>
+// ) : (
+//   null
+// )}
       
-    </div>
+//     </div>
   );
 }
 
